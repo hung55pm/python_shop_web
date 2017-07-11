bd2ac54705124e8f33e446823a84ab1e491e7aeb
MR_HUNG-PC\MR_HUNG <doanngochung55pmxd@gmail.com>
2017-07-11 21:02:05 +0700

    sdsdsds

--- Shop_kiddywatch/web/models.py
+++ Shop_kiddywatch/web/models.py
@@ -6,51 +6,75 @@ from django.db import models
 # Create your models here.
 from django.utils.encoding import python_2_unicode_compatible
 
+
 @python_2_unicode_compatible
 class Category(models.Model):
-    id=models.IntegerField(primary_key=True)
-    name= models.CharField(max_length=50)
-    url=models.CharField(max_length=50, blank=True, null=True)
+    idcate = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    status = models.IntegerField(blank=True, null=True)
+    url = models.CharField(max_length=50, blank=True, null=True)
+    description = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return '{}'.format(self.name)
 
-@python_2_unicode_compatible
-class PhotoProduct(models.Model):
-    id=models.BigAutoField(primary_key=True)
-    name=models.CharField(max_length=50)
-    picture = models.FileField(upload_to='photo/')
-    direct =models.CharField(max_length=100,blank=True, null=True)
-
-
-    def __str__(self):
-        return '{}'.format(self.name)
 
 @python_2_unicode_compatible
 class Product(models.Model):
-    objectid = models.BigAutoField(primary_key=True)
-    title= models.CharField(max_length=50)
-    price= models.CharField(max_length=50)
-    price_sale=models.CharField(max_length=50)
-    status=models.IntegerField(blank=True, null=True)
-    catgory=models.ForeignKey(Category,related_name='Wars',db_column='id')
-    product_code=models.CharField(max_length=50, blank=True, null=True)
-    description=models.CharField(max_length=100, blank=True, null=True)
-    photo=models.ForeignKey(PhotoProduct)
+    idproduct = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    price_sale = models.CharField(max_length=50, blank=True, null=True)
+    idcategory = models.ForeignKey(Category, related_name='cate', db_column='idcate')
+    status = models.IntegerField(blank=True, null=True)
+    product_code = models.CharField(max_length=50, blank=True, null=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return '{}'.format(self.title)
+
+
 @python_2_unicode_compatible
-class PhotoSlideshow(models.Model):
-    objectid = models.BigAutoField(primary_key=True)
-    status = models.IntegerField(blank=True, null=True)
-    direct = models.CharField(max_length=100,blank=True, null=True)
-    picture = models.FileField(upload_to='photo/')
+class SizePhoto(models.Model):
+    idsize = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
+    value = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return '{}'.format(self.name)
 
-class ImageTest(models.Model):
-    objectid = models.BigAutoField(primary_key=True)
-    image=models.ImageField()
+
+@python_2_unicode_compatible
+class PhotoProduct(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    idproduct = models.ForeignKey(Product, related_name='pro', db_column='idproduct')
+    name = models.CharField(max_length=50)
+    picture = models.ImageField()
+    sizephoto = models.ForeignKey(SizePhoto, related_name='sizepro', db_column='idsize')
+    status = models.IntegerField(blank=True, null=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class Slideshow(models.Model):
+    idslide = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    status = models.IntegerField(blank=True, null=True)
+    location = models.CharField(max_length=50, blank=True, null=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
+
+
+@python_2_unicode_compatible
+class PhotoSlideshow(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    idslideshow = models.ForeignKey(Slideshow, related_name='slide', db_column='idslide')
+    picture = models.ImageField()
+    sizephoto = models.ForeignKey(SizePhoto, related_name='sizeslide', db_column='idsize')
+    status = models.IntegerField(blank=True, null=True)
+    description = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return '{}'.format(self.name)
--- db_web_shop.sql
+++ db_web_shop.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: shop_kiddywatch_db
 -- ------------------------------------------------------
@@ -82,7 +82,7 @@ CREATE TABLE `auth_permission` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
   CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,7 +91,7 @@ CREATE TABLE `auth_permission` (
 
 LOCK TABLES `auth_permission` WRITE;
 /*!40000 ALTER TABLE `auth_permission` DISABLE KEYS */;
-INSERT INTO `auth_permission` VALUES (1,'Can add product',1,'add_product'),(2,'Can change product',1,'change_product'),(3,'Can delete product',1,'delete_product'),(4,'Can add photo product',2,'add_photoproduct'),(5,'Can change photo product',2,'change_photoproduct'),(6,'Can delete photo product',2,'delete_photoproduct'),(7,'Can add category',3,'add_category'),(8,'Can change category',3,'change_category'),(9,'Can delete category',3,'delete_category'),(10,'Can add photo slideshow',4,'add_photoslideshow'),(11,'Can change photo slideshow',4,'change_photoslideshow'),(12,'Can delete photo slideshow',4,'delete_photoslideshow'),(13,'Can add log entry',5,'add_logentry'),(14,'Can change log entry',5,'change_logentry'),(15,'Can delete log entry',5,'delete_logentry'),(16,'Can add group',6,'add_group'),(17,'Can change group',6,'change_group'),(18,'Can delete group',6,'delete_group'),(19,'Can add permission',7,'add_permission'),(20,'Can change permission',7,'change_permission'),(21,'Can delete permission',7,'delete_permission'),(22,'Can add user',8,'add_user'),(23,'Can change user',8,'change_user'),(24,'Can delete user',8,'delete_user'),(25,'Can add content type',9,'add_contenttype'),(26,'Can change content type',9,'change_contenttype'),(27,'Can delete content type',9,'delete_contenttype'),(28,'Can add session',10,'add_session'),(29,'Can change session',10,'change_session'),(30,'Can delete session',10,'delete_session');
+INSERT INTO `auth_permission` VALUES (1,'Can add product',1,'add_product'),(2,'Can change product',1,'change_product'),(3,'Can delete product',1,'delete_product'),(4,'Can add size photo',2,'add_sizephoto'),(5,'Can change size photo',2,'change_sizephoto'),(6,'Can delete size photo',2,'delete_sizephoto'),(7,'Can add photo product',3,'add_photoproduct'),(8,'Can change photo product',3,'change_photoproduct'),(9,'Can delete photo product',3,'delete_photoproduct'),(10,'Can add slideshow',4,'add_slideshow'),(11,'Can change slideshow',4,'change_slideshow'),(12,'Can delete slideshow',4,'delete_slideshow'),(13,'Can add category',5,'add_category'),(14,'Can change category',5,'change_category'),(15,'Can delete category',5,'delete_category'),(16,'Can add photo slideshow',6,'add_photoslideshow'),(17,'Can change photo slideshow',6,'change_photoslideshow'),(18,'Can delete photo slideshow',6,'delete_photoslideshow'),(19,'Can add log entry',7,'add_logentry'),(20,'Can change log entry',7,'change_logentry'),(21,'Can delete log entry',7,'delete_logentry'),(22,'Can add group',8,'add_group'),(23,'Can change group',8,'change_group'),(24,'Can delete group',8,'delete_group'),(25,'Can add permission',9,'add_permission'),(26,'Can change permission',9,'change_permission'),(27,'Can delete permission',9,'delete_permission'),(28,'Can add user',10,'add_user'),(29,'Can change user',10,'change_user'),(30,'Can delete user',10,'delete_user'),(31,'Can add content type',11,'add_contenttype'),(32,'Can change content type',11,'change_contenttype'),(33,'Can delete content type',11,'delete_contenttype'),(34,'Can add session',12,'add_session'),(35,'Can change session',12,'change_session'),(36,'Can delete session',12,'delete_session');
 /*!40000 ALTER TABLE `auth_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -116,7 +116,7 @@ CREATE TABLE `auth_user` (
   `date_joined` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,7 +125,6 @@ CREATE TABLE `auth_user` (
 
 LOCK TABLES `auth_user` WRITE;
 /*!40000 ALTER TABLE `auth_user` DISABLE KEYS */;
-INSERT INTO `auth_user` VALUES (1,'pbkdf2_sha256$36000$i8Pm50LvEvCs$MykLUG3dtva+Dc88vBZYU67VJNathth8+sMZJ4IHUKc=','2017-07-11 09:28:47',1,'hungdn','','','hungdn@vinnet.vn',1,1,'2017-07-11 09:22:51');
 /*!40000 ALTER TABLE `auth_user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -206,7 +205,7 @@ CREATE TABLE `django_admin_log` (
   KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
   CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
   CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -215,7 +214,6 @@ CREATE TABLE `django_admin_log` (
 
 LOCK TABLES `django_admin_log` WRITE;
 /*!40000 ALTER TABLE `django_admin_log` DISABLE KEYS */;
-INSERT INTO `django_admin_log` VALUES (1,'2017-07-11 09:29:05','1','Đồng hồ kiddy',1,'[{\"added\": {}}]',3,1),(2,'2017-07-11 09:29:25','2','Sim kiddy',1,'[{\"added\": {}}]',3,1),(3,'2017-07-11 09:30:11','3','Dây và củ sạc',1,'[{\"added\": {}}]',3,1),(4,'2017-07-11 09:31:47','1','photo slideshow 1',1,'[{\"added\": {}}]',4,1),(5,'2017-07-11 09:32:19','1','photo slideshow 1',2,'[{\"changed\": {\"fields\": [\"picture\"]}}]',4,1),(6,'2017-07-11 09:33:23','1','photo slideshow 1',2,'[{\"changed\": {\"fields\": [\"picture\"]}}]',4,1),(7,'2017-07-11 09:33:38','1','photo slideshow 1',2,'[]',4,1),(8,'2017-07-11 09:33:49','2','photo slideshow 2',1,'[{\"added\": {}}]',4,1),(9,'2017-07-11 09:36:00','1','Đồng hồ kiddy xanh và sim kid30',1,'[{\"added\": {}}]',2,1),(10,'2017-07-11 09:36:44','2','Đồng hồ kiddy đen và sim kid30',1,'[{\"added\": {}}]',2,1),(11,'2017-07-11 09:37:09','3','Đồng hồ kiddy hồng và sim kid30',1,'[{\"added\": {}}]',2,1),(12,'2017-07-11 09:37:47','4','Sim kiddy30',1,'[{\"added\": {}}]',2,1),(13,'2017-07-11 09:39:12','1','ĐỒNG HỒ KIDDY MÀU HỒNG + SIM KID30',1,'[{\"added\": {}}]',1,1),(14,'2017-07-11 09:39:40','2','ĐỒNG HỒ KIDDY MÀU XANH + SIM KID30',1,'[{\"added\": {}}]',1,1),(15,'2017-07-11 09:40:03','3','ĐỒNG HỒ KIDDY MÀU ĐEN + SIM KID30',1,'[{\"added\": {}}]',1,1),(16,'2017-07-11 09:40:57','4','SIM KID30 - K30',1,'[{\"added\": {}}]',1,1),(17,'2017-07-11 09:41:41','3','Đồng hồ kiddy hồng và sim kid30',2,'[{\"changed\": {\"fields\": [\"direct\"]}}]',2,1),(18,'2017-07-11 09:41:50','2','Đồng hồ kiddy đen và sim kid30',2,'[{\"changed\": {\"fields\": [\"direct\"]}}]',2,1),(19,'2017-07-11 09:42:48','2','photo slideshow 2',2,'[{\"changed\": {\"fields\": [\"status\"]}}]',4,1);
 /*!40000 ALTER TABLE `django_admin_log` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -232,7 +230,7 @@ CREATE TABLE `django_content_type` (
   `model` varchar(100) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -241,7 +239,7 @@ CREATE TABLE `django_content_type` (
 
 LOCK TABLES `django_content_type` WRITE;
 /*!40000 ALTER TABLE `django_content_type` DISABLE KEYS */;
-INSERT INTO `django_content_type` VALUES (5,'admin','logentry'),(6,'auth','group'),(7,'auth','permission'),(8,'auth','user'),(9,'contenttypes','contenttype'),(10,'sessions','session'),(3,'web','category'),(2,'web','photoproduct'),(4,'web','photoslideshow'),(1,'web','product');
+INSERT INTO `django_content_type` VALUES (7,'admin','logentry'),(8,'auth','group'),(9,'auth','permission'),(10,'auth','user'),(11,'contenttypes','contenttype'),(12,'sessions','session'),(5,'web','category'),(3,'web','photoproduct'),(6,'web','photoslideshow'),(1,'web','product'),(2,'web','sizephoto'),(4,'web','slideshow');
 /*!40000 ALTER TABLE `django_content_type` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -267,7 +265,7 @@ CREATE TABLE `django_migrations` (
 
 LOCK TABLES `django_migrations` WRITE;
 /*!40000 ALTER TABLE `django_migrations` DISABLE KEYS */;
-INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2017-07-11 09:21:57'),(2,'auth','0001_initial','2017-07-11 09:22:06'),(3,'admin','0001_initial','2017-07-11 09:22:08'),(4,'admin','0002_logentry_remove_auto_add','2017-07-11 09:22:08'),(5,'contenttypes','0002_remove_content_type_name','2017-07-11 09:22:09'),(6,'auth','0002_alter_permission_name_max_length','2017-07-11 09:22:10'),(7,'auth','0003_alter_user_email_max_length','2017-07-11 09:22:11'),(8,'auth','0004_alter_user_username_opts','2017-07-11 09:22:11'),(9,'auth','0005_alter_user_last_login_null','2017-07-11 09:22:11'),(10,'auth','0006_require_contenttypes_0002','2017-07-11 09:22:11'),(11,'auth','0007_alter_validators_add_error_messages','2017-07-11 09:22:11'),(12,'auth','0008_alter_user_username_max_length','2017-07-11 09:22:12'),(13,'sessions','0001_initial','2017-07-11 09:22:12'),(14,'web','0001_initial','2017-07-11 09:22:15');
+INSERT INTO `django_migrations` VALUES (1,'contenttypes','0001_initial','2017-07-11 13:58:37'),(2,'auth','0001_initial','2017-07-11 13:58:44'),(3,'admin','0001_initial','2017-07-11 13:58:46'),(4,'admin','0002_logentry_remove_auto_add','2017-07-11 13:58:46'),(5,'contenttypes','0002_remove_content_type_name','2017-07-11 13:58:47'),(6,'auth','0002_alter_permission_name_max_length','2017-07-11 13:58:48'),(7,'auth','0003_alter_user_email_max_length','2017-07-11 13:58:48'),(8,'auth','0004_alter_user_username_opts','2017-07-11 13:58:48'),(9,'auth','0005_alter_user_last_login_null','2017-07-11 13:58:49'),(10,'auth','0006_require_contenttypes_0002','2017-07-11 13:58:49'),(11,'auth','0007_alter_validators_add_error_messages','2017-07-11 13:58:49'),(12,'auth','0008_alter_user_username_max_length','2017-07-11 13:58:50'),(13,'sessions','0001_initial','2017-07-11 13:58:51'),(14,'web','0001_initial','2017-07-11 13:58:58');
 /*!40000 ALTER TABLE `django_migrations` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -293,7 +291,6 @@ CREATE TABLE `django_session` (
 
 LOCK TABLES `django_session` WRITE;
 /*!40000 ALTER TABLE `django_session` DISABLE KEYS */;
-INSERT INTO `django_session` VALUES ('86m9d6qqqs4tuy57k5n9kn9gaebg644w','YTFiZTc1N2JhMjQwNzZkMWM4ZGZlZDk5NDRkZTFlNmY4M2Y4ZDhhNTp7Il9hdXRoX3VzZXJfaGFzaCI6IjgwY2I5MmZiZWFhMDQzNjQ3ODdkNzE3YjExOTBiZTgxZTVkNmUzMmUiLCJfYXV0aF91c2VyX2JhY2tlbmQiOiJkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZCIsIl9hdXRoX3VzZXJfaWQiOiIxIn0=','2017-07-25 09:23:08'),('hly9ixw7vrgiyb5w95w2qfwcynwrtewm','YTFiZTc1N2JhMjQwNzZkMWM4ZGZlZDk5NDRkZTFlNmY4M2Y4ZDhhNTp7Il9hdXRoX3VzZXJfaGFzaCI6IjgwY2I5MmZiZWFhMDQzNjQ3ODdkNzE3YjExOTBiZTgxZTVkNmUzMmUiLCJfYXV0aF91c2VyX2JhY2tlbmQiOiJkamFuZ28uY29udHJpYi5hdXRoLmJhY2tlbmRzLk1vZGVsQmFja2VuZCIsIl9hdXRoX3VzZXJfaWQiOiIxIn0=','2017-07-25 09:28:47');
 /*!40000 ALTER TABLE `django_session` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -305,10 +302,12 @@ DROP TABLE IF EXISTS `web_category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `web_category` (
-  `id` int(11) NOT NULL,
+  `idcate` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
+  `status` int(11) DEFAULT NULL,
   `url` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `description` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idcate`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -318,7 +317,6 @@ CREATE TABLE `web_category` (
 
 LOCK TABLES `web_category` WRITE;
 /*!40000 ALTER TABLE `web_category` DISABLE KEYS */;
-INSERT INTO `web_category` VALUES (1,'Đồng hồ kiddy',NULL),(2,'Sim kiddy',NULL),(3,'Dây và củ sạc',NULL);
 /*!40000 ALTER TABLE `web_category` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -333,9 +331,16 @@ CREATE TABLE `web_photoproduct` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
   `picture` varchar(100) NOT NULL,
-  `direct` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `status` int(11) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `idproduct` bigint(20) NOT NULL,
+  `idsize` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `web_photoproduct_idproduct_9971a2ea_fk_web_product_idproduct` (`idproduct`),
+  KEY `web_photoproduct_idsize_11db715c_fk_web_sizephoto_idsize` (`idsize`),
+  CONSTRAINT `web_photoproduct_idproduct_9971a2ea_fk_web_product_idproduct` FOREIGN KEY (`idproduct`) REFERENCES `web_product` (`idproduct`),
+  CONSTRAINT `web_photoproduct_idsize_11db715c_fk_web_sizephoto_idsize` FOREIGN KEY (`idsize`) REFERENCES `web_sizephoto` (`idsize`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -344,7 +349,6 @@ CREATE TABLE `web_photoproduct` (
 
 LOCK TABLES `web_photoproduct` WRITE;
 /*!40000 ALTER TABLE `web_photoproduct` DISABLE KEYS */;
-INSERT INTO `web_photoproduct` VALUES (1,'Đồng hồ kiddy xanh và sim kid30','photo/xanh_sim.jpg','/media/photo/xanh_sim.jpg'),(2,'Đồng hồ kiddy đen và sim kid30','photo/back_sim.png','/media/photo/back_sim.png'),(3,'Đồng hồ kiddy hồng và sim kid30','photo/hong_sim.png','/media/photo/hong_sim.png'),(4,'Sim kiddy30','photo/sim_kiddy30.jpg','/media/photo/sim_kiddy30.jpg');
 /*!40000 ALTER TABLE `web_photoproduct` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -356,13 +360,19 @@ DROP TABLE IF EXISTS `web_photoslideshow`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `web_photoslideshow` (
-  `objectid` bigint(20) NOT NULL AUTO_INCREMENT,
-  `status` int(11) DEFAULT NULL,
-  `direct` varchar(100) DEFAULT NULL,
-  `picture` varchar(100) NOT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  PRIMARY KEY (`objectid`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `picture` varchar(100) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  `idslide` bigint(20) NOT NULL,
+  `idsize` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `web_photoslideshow_idslide_5d953bda_fk_web_slideshow_idslide` (`idslide`),
+  KEY `web_photoslideshow_idsize_42c1c393_fk_web_sizephoto_idsize` (`idsize`),
+  CONSTRAINT `web_photoslideshow_idsize_42c1c393_fk_web_sizephoto_idsize` FOREIGN KEY (`idsize`) REFERENCES `web_sizephoto` (`idsize`),
+  CONSTRAINT `web_photoslideshow_idslide_5d953bda_fk_web_slideshow_idslide` FOREIGN KEY (`idslide`) REFERENCES `web_slideshow` (`idslide`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -371,7 +381,6 @@ CREATE TABLE `web_photoslideshow` (
 
 LOCK TABLES `web_photoslideshow` WRITE;
 /*!40000 ALTER TABLE `web_photoslideshow` DISABLE KEYS */;
-INSERT INTO `web_photoslideshow` VALUES (1,1,'/media/photo/banner_1.jpg','photo/banner_1.jpg','photo slideshow 1'),(2,1,'/media/photo/banner_2.jpg','photo/banner_2.jpg','photo slideshow 2');
 /*!40000 ALTER TABLE `web_photoslideshow` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -383,21 +392,18 @@ DROP TABLE IF EXISTS `web_product`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `web_product` (
-  `objectid` bigint(20) NOT NULL AUTO_INCREMENT,
+  `idproduct` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   `price` varchar(50) NOT NULL,
-  `price_sale` varchar(50) NOT NULL,
+  `price_sale` varchar(50) DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
   `product_code` varchar(50) DEFAULT NULL,
   `description` varchar(100) DEFAULT NULL,
-  `id` int(11) NOT NULL,
-  `photo_id` bigint(20) NOT NULL,
-  PRIMARY KEY (`objectid`),
-  KEY `web_product_id_662bb9ce_fk_web_category_id` (`id`),
-  KEY `web_product_photo_id_c2d231c3_fk_web_photoproduct_id` (`photo_id`),
-  CONSTRAINT `web_product_id_662bb9ce_fk_web_category_id` FOREIGN KEY (`id`) REFERENCES `web_category` (`id`),
-  CONSTRAINT `web_product_photo_id_c2d231c3_fk_web_photoproduct_id` FOREIGN KEY (`photo_id`) REFERENCES `web_photoproduct` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+  `idcate` bigint(20) NOT NULL,
+  PRIMARY KEY (`idproduct`),
+  KEY `web_product_idcate_154df65c_fk_web_category_idcate` (`idcate`),
+  CONSTRAINT `web_product_idcate_154df65c_fk_web_category_idcate` FOREIGN KEY (`idcate`) REFERENCES `web_category` (`idcate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -406,8 +412,57 @@ CREATE TABLE `web_product` (
 
 LOCK TABLES `web_product` WRITE;
 /*!40000 ALTER TABLE `web_product` DISABLE KEYS */;
-INSERT INTO `web_product` VALUES (1,'ĐỒNG HỒ KIDDY MÀU HỒNG + SIM KID30','1.455.000đ','1.355.000đ',1,'K01',NULL,1,3),(2,'ĐỒNG HỒ KIDDY MÀU XANH + SIM KID30','1.455.000đ','1.355.000đ',1,'K01',NULL,1,1),(3,'ĐỒNG HỒ KIDDY MÀU ĐEN + SIM KID30','1.455.000đ','1.355.000đ',1,'K01',NULL,1,2),(4,'SIM KID30 - K30','65.000đ','65.000đ',1,'SC/KID30',NULL,2,4);
 /*!40000 ALTER TABLE `web_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `web_sizephoto`
+--
+
+DROP TABLE IF EXISTS `web_sizephoto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `web_sizephoto` (
+  `idsize` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `value` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idsize`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `web_sizephoto`
+--
+
+LOCK TABLES `web_sizephoto` WRITE;
+/*!40000 ALTER TABLE `web_sizephoto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `web_sizephoto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `web_slideshow`
+--
+
+DROP TABLE IF EXISTS `web_slideshow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `web_slideshow` (
+  `idslide` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `location` varchar(50) DEFAULT NULL,
+  `description` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`idslide`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `web_slideshow`
+--
+
+LOCK TABLES `web_slideshow` WRITE;
+/*!40000 ALTER TABLE `web_slideshow` DISABLE KEYS */;
+/*!40000 ALTER TABLE `web_slideshow` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -419,4 +474,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-07-11 17:29:42
+-- Dump completed on 2017-07-11 21:00:03
